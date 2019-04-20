0d91e31d057ceeb16405c466325c9a35b6f6f125
TaeGeun Moon <ahn_v3@naver.com>
2019-04-20 12:19:31 +0900

    Add apollo

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const next = require('next');
+require('dotenv').config();
 const {
   ApolloServer,
   gql
 } = require('apollo-server-express');
+const MongoClient = require('mongodb').MongoClient;
+const mongo = new MongoClient(process.env.MONGO_URL, {
+  useNewUrlParser: true
+});
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({
@@ -13,14 +18,23 @@ const handle = app.getRequestHandler();
 
 const typeDefs = gql `
   type Query {
-    hello: String
+    article: [Article]
+  }
+  type Article {
+    title: String
+    content: String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    article: () =>
+      mongo.db('blog').collection('article').find().toArray(),
   },
+  Article: {
+    title: (article) => article.title,
+    content: (article) => article.content
+  }
 };
 
 const apolloServer = new ApolloServer({
@@ -33,17 +47,20 @@ app
   .prepare()
   .then(() => {
     const server = express();
+    mongo.connect(err => {
+      if (err) console.log(err)
+    });
     apolloServer.applyMiddleware({
       app: server
     });
-    const port = process.env.PORT || 8080;
+    const port = process.env.PORT || 3000;
     server.get('*', (req, res) => {
       return handle(req, res);
     });
 
     server.listen(port, err => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+      console.log(` > Ready on http: //localhost:3000${apolloServer.graphqlPath}`);
     });
   })
   .catch(ex => {
