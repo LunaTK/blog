d323a6ebac1416a37cee33fa0e30fd597dbe4be1
TaeGeun Moon <ahn_v3@naver.com>
2019-05-07 12:27:30 +0900

    remove unnecessaries

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,6 @@ function index() {
   return (
     <Layout>
       <div>Hi</div>
-      <Link prefetch as="/post/타이틀" href={`/post?title=${'타이틀'}`}>
-        <a> GOGO </a>
-      </Link>
       <br />
       <Link href={`/lab`}>
         <a>GO to Lab</a>
--- pages/post.tsx
+++ pages/post.tsx
@@ -6,7 +6,6 @@ import Post from '../components/Post';
 const post = withRouter(props => {
   return (
     <Layout>
-      <h1>포스트 에디터 페이지</h1>
       <Post postId={(props.router.query as any).id} />
     </Layout>
   );
