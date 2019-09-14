b6a826d30b06142b958e30be0122662f7a82a939
TaeGeun Moon <ahn_v3@naver.com>
2019-09-14 13:55:11 +0900

    Update dependencies' version

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
-const withSASS = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-const withTypescript = require('@zeit/next-typescript')
-const withImages = require('next-images')
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
-module.exports = withImages(withTypescript(withSASS(withCSS(withBundleAnalyzer({
-    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-        server: {
+const withSASS = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
+const withImages = require('next-images');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+module.exports = withImages(
+  withSASS(
+    withCSS(
+      withBundleAnalyzer({
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        bundleAnalyzerConfig: {
+          server: {
             analyzerMode: 'static',
             reportFilename: '../bundles/server.html'
-        },
-        browser: {
+          },
+          browser: {
             analyzerMode: 'static',
             reportFilename: '../bundles/client.html'
+          }
         }
-    }
-})))));
+      })
+    )
+  )
+);
